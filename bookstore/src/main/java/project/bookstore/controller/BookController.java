502d63aaf25b70d4aa945c33cfe139c5d11ee212
huongduyen <duyen.vanthi.cit20@eiu.edu.vn>
2024-02-20 19:46:22 +0700

    update project
    update link in Client

--- bookstore/src/main/java/project/bookstore/controller/BookController.java
+++ bookstore/src/main/java/project/bookstore/controller/BookController.java
@@ -1,32 +1,32 @@
 // package project.bookstore.controller;
-
+//
 // import java.util.*;
-
+//
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.stereotype.Controller;
 // import org.springframework.ui.Model;
 // import org.springframework.web.bind.annotation.*;
 // import org.springframework.web.servlet.ModelAndView;
-
+//
 // import project.bookstore.entity.Book;
 // import project.bookstore.service.BookService;
-
+//
 // @Controller
 // public class BookController {
-
+//
 //     @Autowired
 //     private BookService service;
-
+//
 //     @GetMapping("/")
 //     public String home() {
 //         return "Admin/admin-dashboard";
 //     }
-
+//
 //     @GetMapping("/addNewBook")
 //     public String bookRegister() {
 //         return "admin-add-book";
 //     }
-
+//
 //     @GetMapping("/adminBooks")
 //     public ModelAndView getAllBook() {
 //         List<Book> list = service.getAllBook();
@@ -36,20 +36,20 @@
 //         // return modelAndView;
 //         return new ModelAndView("admin-books", "book", list);
 //     }
-
+//
 //     @PostMapping("/save")
 //     public String addBook(@ModelAttribute Book book) {
 //         service.save(book);
 //         return "redirect:/admin-books";
 //     }
-
+//
 //     @GetMapping("/my_books")
 //     public String getMyBooks(Model model) {
 //         List<MyBookList> bookList = myBookService.getAllMyBooks();
 //         model.addAttribute("book", bookList);
 //         return "myBooks";
 //     }
-
+//
 //     @RequestMapping("/mylist/{id}")
 //     public String getMyList(@PathVariable("id") int id) {
 //         Book book = service.getBookById(id);
@@ -57,18 +57,18 @@
 //         myBookService.saveMyBooks(bookList);
 //         return "redirect:/my_books";
 //     }
-
+//
 //     @RequestMapping("/editBook/{id}")
 // 	public String editBook(@PathVariable("id") int id,Model model) {
 // 		Book b=service.getBookById(id);
 // 		model.addAttribute("book",b);
 // 		return "bookEdit";
 // 	}
-
+//
 // 	@RequestMapping("/deleteBook/{id}")
 // 	public String deleteBook(@PathVariable("id")int id) {
 // 		service.deleteById(id);
 // 		return "redirect:/admin-books.html";
 // 	}
-
+//
 // }
