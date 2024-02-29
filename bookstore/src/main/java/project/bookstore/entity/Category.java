a676d943f5434508c22030b9a88a43b3fad37675
huongduyen <duyen.vanthi.cit20@eiu.edu.vn>
2024-02-29 19:41:42 +0700

    update project
    create crud in category

--- bookstore/src/main/java/project/bookstore/entity/Category.java
+++ bookstore/src/main/java/project/bookstore/entity/Category.java
@@ -1,12 +1,9 @@
 package project.bookstore.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "category")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,10 +17,6 @@ public class Category {
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
