eb654b433aaa54dd2e389b21470205ea49378a78
kiet.luong <kiet.luong.cit20@eiu.edu.vn>
2024-04-30 22:40:52 +0700

    Update AddressRepository.java

--- bookstore/src/main/java/project/bookstore/repository/AddressRepository.java
+++ bookstore/src/main/java/project/bookstore/repository/AddressRepository.java
@@ -3,5 +3,5 @@ package project.bookstore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.bookstore.entity.Address;
 
-public interface AddressRepository extends JpaRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
